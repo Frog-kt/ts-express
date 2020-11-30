a444f7f8f4e9ecc73bacf4dd03a9eaf8c32a6320
Frog-kt <kasuo.takumi@gmail.com>
2020-11-30 15:35:28 +0900

    Add jest & supertest
    TDDするためにjestとsupertestを追加

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  roots: ['<rootDir>/test'],
+
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+
+  testRegex: '(/test/.*|(\\.|/)(test|spec))\\.tsx?$',
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
